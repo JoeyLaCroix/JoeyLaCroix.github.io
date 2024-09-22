9d2c8e4817b8cb8787b3ddfc6874ce9c4bb27658
joey LaCroix <JLACROIX@email.sc.edu>
2024-09-22 17:33:40 -0400

    new

--- csce242/assignments/homework06-Conditionals/script.js
+++ csce242/assignments/homework06-Conditionals/script.js
@@ -1,9 +1,51 @@
-/*whenever one link in your nav bar is clicked to display its respective box, the other box should be assigned a class that contains a display: none attribute within it
-*/
+
+const btn = document.getElementById("btn")
+
 document.getElementById("ass-1").addEventListener("click", function(){
   document.getElementById("slider").style.display = "block";
   document.getElementById("images").style.display = "none";
 });
+document.getElementById("ass-2").addEventListener("click", function(){
+  document.getElementById("slider").style.display = "none";
+  document.getElementById("images").style.display = "block";
+});
+
+btn.onclick = () =>{
+  
+  if(btn.innerHTML=="Small"){
+  document.getElementById("img-small").style.display = "block";
+  document.getElementById("img-medium").style.display = "none";
+  document.getElementById("img-large").style.display = "none";
+  }
+  else if(btn.innerHTML=="Medium"){
+    document.getElementById("img-small").style.display = "none";
+    document.getElementById("img-medium").style.display = "block";
+    document.getElementById("img-large").style.display = "none";
+    }
+    else if(btn.innerHTML=="Large"){
+      document.getElementById("img-small").style.display = "none";
+      document.getElementById("img-medium").style.display = "none";
+      document.getElementById("img-large").style.display = "block";
+      }
+}
+
+
+/*btnlarge.onclick = () =>{
+  document.getElementById("img-large").style.display = "block";
+  document.getElementById("img-medium").style.display = "none";
+  document.getElementById("img-small").style.display = "none";
+}
+btnmedium.onclick = () =>{
+  document.getElementById("img-medium").style.display = "block";
+  document.getElementById("img-small").style.display = "none";
+  document.getElementById("img-large").style.display = "none";
+}
+btnsmall.onclick = () =>{
+  document.getElementById("img-small").style.display = "block";
+  document.getElementById("img-medium").style.display = "none";
+  document.getElementById("img-large").style.display = "none";
+}*/
+
 
 
 
