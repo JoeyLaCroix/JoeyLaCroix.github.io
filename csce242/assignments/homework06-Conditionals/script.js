55f8354d85f0008963eedb3400aeae9938607410
joey LaCroix <JLACROIX@email.sc.edu>
2024-09-22 14:14:58 -0400

    new

--- csce242/assignments/homework06-Conditionals/script.js
+++ csce242/assignments/homework06-Conditionals/script.js
@@ -1,8 +1,9 @@
-/*When a link in the nav is clicked a box apears below*/
-const link = document.getElementById("ass-1");
+/*whenever one link in your nav bar is clicked to display its respective box, the other box should be assigned a class that contains a display: none attribute within it
+*/
+document.getElementById("ass-1").addEventListener("click", function(){
+  document.getElementById("slider").style.display = "block";
+  document.getElementById("images").style.display = "none";
+});
 
-link.onclick = () =>{
-    console.log("true");
 
-}
 
