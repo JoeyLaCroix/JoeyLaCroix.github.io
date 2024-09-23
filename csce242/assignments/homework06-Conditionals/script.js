5a3249274e4826f3db1bea8fa49a72585d1893a9
joey LaCroix <JLACROIX@email.sc.edu>
2024-09-22 22:40:29 -0400

    final

--- csce242/assignments/homework06-Conditionals/script.js
+++ csce242/assignments/homework06-Conditionals/script.js
@@ -2,8 +2,19 @@
 const btnsmall = document.getElementById("btn-small");
 const btnmedium = document.getElementById("btn-medium");
 const btnlarge = document.getElementById("btn-large");
-const slider = document.getElementById("slider");
+const slider = document.getElementById("slider1");
 const text = document.getElementById("text");
+const sliderbox = document.getElementById("slider");
+
+
+const toggleNav = () => {
+  document.getElementById("main-items").classList.toggle("hidden");
+}
+
+window.onload = () => {
+  document.getElementById("nav-toggle").onclick = toggleNav;
+}
+
 
 document.getElementById("ass-1").addEventListener("click", function(){
   document.getElementById("slider").style.display = "block";
@@ -18,15 +29,15 @@ document.getElementById("ass-2").addEventListener("click", function(){
 btnsmall.onclick = () => {
   document.getElementById("img-large").style.display = "none";
   document.getElementById("img-medium").style.display = "none";
-  document.getElementById("img-small").style.display = "block";
+  document.getElementById("img-small").style.display = "inline";
 }
 btnmedium.onclick = () => {
   document.getElementById("img-large").style.display = "none";
-  document.getElementById("img-medium").style.display = "block";
+  document.getElementById("img-medium").style.display = "inline";
   document.getElementById("img-small").style.display = "none";
 }
 btnlarge.onclick = () => {
-  document.getElementById("img-large").style.display = "block";
+  document.getElementById("img-large").style.display = "inline";
   document.getElementById("img-medium").style.display = "none";
   document.getElementById("img-small").style.display = "none";
 }
@@ -35,14 +46,14 @@ btnlarge.onclick = () => {
 
 slider.addEventListener('input', function() {
     const redValue = slider.value;
-    document.body.style.backgroundColor = `rgb(${redValue}, 0, 0)`;
+    sliderbox.style.backgroundColor = `rgb(${redValue}, 0, 0)`;
   
     if (redValue < 85) {
         text.innerHTML = "Your not warm yet!";
     } else if (redValue < 170) {
-        message.innerHTML = "its heating up!";
+        text.innerHTML = "its heating up!";
     } else {
-        message.innerHTML = "Your burning up!";
+        text.innerHTML = "Your burning up!";
     }
 });
 
