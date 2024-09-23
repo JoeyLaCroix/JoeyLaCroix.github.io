d892e5cfa42f5d0a903ee904e210d51fec966d91
joey LaCroix <JLACROIX@email.sc.edu>
2024-09-22 21:22:52 -0400

    new

--- csce242/assignments/homework06-Conditionals/script.js
+++ csce242/assignments/homework06-Conditionals/script.js
@@ -1,5 +1,9 @@
 
-const btn = document.getElementById("btn")
+const btnsmall = document.getElementById("btn-small");
+const btnmedium = document.getElementById("btn-medium");
+const btnlarge = document.getElementById("btn-large");
+const slider = document.getElementById("slider");
+const text = document.getElementById("text");
 
 document.getElementById("ass-1").addEventListener("click", function(){
   document.getElementById("slider").style.display = "block";
@@ -10,41 +14,38 @@ document.getElementById("ass-2").addEventListener("click", function(){
   document.getElementById("images").style.display = "block";
 });
 
-btn.onclick = () =>{
-  
-  if(btn.innerHTML=="Small"){
-  document.getElementById("img-small").style.display = "block";
-  document.getElementById("img-medium").style.display = "none";
+
+btnsmall.onclick = () => {
   document.getElementById("img-large").style.display = "none";
-  }
-  else if(btn.innerHTML=="Medium"){
-    document.getElementById("img-small").style.display = "none";
-    document.getElementById("img-medium").style.display = "block";
-    document.getElementById("img-large").style.display = "none";
-    }
-    else if(btn.innerHTML=="Large"){
-      document.getElementById("img-small").style.display = "none";
-      document.getElementById("img-medium").style.display = "none";
-      document.getElementById("img-large").style.display = "block";
-      }
+  document.getElementById("img-medium").style.display = "none";
+  document.getElementById("img-small").style.display = "block";
 }
-
-
-/*btnlarge.onclick = () =>{
+btnmedium.onclick = () => {
+  document.getElementById("img-large").style.display = "none";
+  document.getElementById("img-medium").style.display = "block";
+  document.getElementById("img-small").style.display = "none";
+}
+btnlarge.onclick = () => {
   document.getElementById("img-large").style.display = "block";
   document.getElementById("img-medium").style.display = "none";
   document.getElementById("img-small").style.display = "none";
 }
-btnmedium.onclick = () =>{
-  document.getElementById("img-medium").style.display = "block";
-  document.getElementById("img-small").style.display = "none";
-  document.getElementById("img-large").style.display = "none";
-}
-btnsmall.onclick = () =>{
-  document.getElementById("img-small").style.display = "block";
-  document.getElementById("img-medium").style.display = "none";
-  document.getElementById("img-large").style.display = "none";
-}*/
+
+
+
+slider.addEventListener('input', function() {
+    const redValue = slider.value;
+    document.body.style.backgroundColor = `rgb(${redValue}, 0, 0)`;
+  
+    if (redValue < 85) {
+        text.innerHTML = "Your not warm yet!";
+    } else if (redValue < 170) {
+        message.innerHTML = "its heating up!";
+    } else {
+        message.innerHTML = "Your burning up!";
+    }
+});
+
 
 
 
