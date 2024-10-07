18f1a1721110359e93e9acc97e916c58c236be53
joey LaCroix <JLACROIX@email.sc.edu>
2024-10-06 22:10:10 -0400

    classes

--- csce242/assignments/homework09-classes/script.js
+++ csce242/assignments/homework09-classes/script.js
@@ -12,7 +12,7 @@ class Bird {
 
     getSection() {
         const section = document.createElement('div');
-        section.className = 'bird-box';
+        section.className = 'bird-section';
         section.onclick = () => showDetails(this.name);
 
         const title = document.createElement('h3');
@@ -35,37 +35,44 @@ class Bird {
         title.innerText = this.name;
         container.appendChild(title);
 
+        
+
+        const ul = document.createElement('ul');
+        ul.id = 'ul';
+        container.appendChild(ul);
+
         const img = document.createElement('img');
-        img.src = this.image;
-        img.alt = this.name;
-        container.appendChild(img);
+           img.classList.add("img");
+            img.src = this.image;
+            img.alt = this.name;
+            container.appendChild(img);
 
-        const size = document.createElement('p');
-        size.innerHTML = "<strong>Size:</strong> " + this.size;
-        container.appendChild(size);
+        const size = document.createElement('li');
+        size.innerHTML = "<strong>   Size:</strong> " + this.size;
+        ul.appendChild(size);
 
-        const lifespan = document.createElement('p');
-        lifespan.innerHTML = "<strong>Lifespan:</strong> " + this.lifespan;
-        container.appendChild(lifespan);
+        const lifespan = document.createElement('li');
+        lifespan.innerHTML = "<strong>   Lifespan:</strong> " + this.lifespan;
+        ul.appendChild(lifespan);
 
-        const food = document.createElement('p');
-        food.innerHTML = "<strong>Food:</strong> " + this.food;
-        container.appendChild(food);
+        const food = document.createElement('li');
+        food.innerHTML = "<strong>   Food:</strong> " + this.food;
+        ul.appendChild(food);
 
-        const habitat = document.createElement('p');
-        habitat.innerHTML = "<strong>Habitat:</strong> " + this.habitat;
-        container.appendChild(habitat);
+        const habitat = document.createElement('li');
+        habitat.innerHTML = "<strong>   Habitat:</strong> " + this.habitat;
+        ul.appendChild(habitat);
 
-        const fact = document.createElement('p');
-        fact.innerHTML = "<strong>Interesting Fact:</strong> " + this.fact;
-        container.appendChild(fact);
+        const fact = document.createElement('li');
+        fact.innerHTML = "<strong>   Interesting Fact:</strong> " + this.fact;
+        ul.appendChild(fact);
 
         return container;
     }
 }
 
 const birds = [
-    new Bird('Bald Eagle', 'images/baldeagle.jpg', '2.5 feet', '10-20 years', 'Small rodents and fish', 'Trees', 'They are the national bird of the USA'),
+    new Bird('   Bald Eagle   ','   images/baldeagle.jpg    ', '   2.5 feet   ', '   10-20 years   ', '   Small rodents and fish   ', '   Trees   ', '   They are the national bird of the USA   '),
     new Bird('Flamingo', 'images/flamingo.webp', '3-4.5 feet', '20-25 years', 'shrimp', 'marshes', 'They are naturally white'),
     new Bird('Osprey', 'images/osprey.jpg', '1.5 feet', '15', 'small rodents and fish', 'coastlines', 'They are also known as sea hawks'),
     new Bird('Ostrich', 'images/ostrich.jpg', '6-9 feet', '20 years', 'grass, greens and flowery plants', 'sierras and sarahas', 'They are the largest bird in the world'),
@@ -80,7 +87,7 @@ birds.forEach(bird => {
 
 function showDetails(birdName) {
     const bird = birds.find(b => b.name === birdName);
-    document.getElementById('modalTitle').innerText = bird.name;
+    document.getElementById('Title').innerText = bird.name;
 
     const modalContent = document.getElementById('modalContent');
     modalContent.innerHTML = '';
