9e5f909862389ce4c949ac5185a344c6f41ac834
joey LaCroix <JLACROIX@email.sc.edu>
2024-10-13 20:54:55 -0400

    new

--- csce242/projects/part four/script.js
+++ csce242/projects/part four/script.js
@@ -5,3 +5,35 @@ const toggleNav = () => {
 window.onload = () => {
     document.getElementById("nav-toggle").onclick = toggleNav;
 }
+
+const getSurfers = async() => {
+    const url = "C:\Users\joela\OneDrive\Desktop\csce242\JoeyLaCroix.github.io\projects\part four\surfers.json";
+
+    try{
+        const response = await fetch(url);
+        return response.json();
+    } catch(error){
+        console.log(error);
+    }
+};
+
+const showShurfers = async() => {
+    const surfers = await getSurfers();
+    surfers.forEach((surfer)=>{
+        document.getElementById("main-surf-list").append(getSurfersSection(surfer));
+    });
+};
+
+const getSurfersSection = (surfer) => {
+    const section = document.createElement("section");
+    section.classList.add("surfer");
+    const h3 = document.createElement("h3");
+    const p = document.createElement("p");
+
+    h3.innerHTML = surfer.name;
+    p.innerHTML = surfer.bio;
+
+    section.append(h3);
+    section.append(p);
+    document.getElementById("main-surf-list").append(section);
+};
