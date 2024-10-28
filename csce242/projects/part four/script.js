8d577c8245ff10b79adf5592c568c51cc7249112
joey LaCroix <JLACROIX@email.sc.edu>
2024-10-27 23:05:00 -0400

    new

--- csce242/projects/part four/script.js
+++ csce242/projects/part four/script.js
@@ -31,18 +31,39 @@ const getSurfersSection = (surfer) => {
     const p = document.createElement("p");
     const h4 = document.createElement("h4");
     const img = document.createElement("img");
+    const h4t = document.createElement("h4");
 
-    img.src = surfer.img_name;
+    img.src = "images/"+surfer.img_name;
     h4.innerHTML = surfer.hometown;
+    h4t.innerHTML = "Surf Type: "+surfer.surftype;
     h3.innerHTML = surfer.name;
     p.innerHTML = surfer.bio;
 
     section.append(img);
     section.append(h3);
     section.append(h4);
+    section.append(h4t);
     section.append(p);
     
     return section;
 };
 
 showShurfers();
+
+document.getElementById("contactUsForm").onsubmit = (e) => {
+    e.preventDefault();//dont refresh page
+
+    const form = e.target;
+
+    const name = form.elements["name"].value;
+    const email = form.elements["email"].value;
+    const surferr = form.elements["surfer?"].value;
+    const message = form.elements["message"].value;
+
+    console.log(`${name} is a ${surferr} with a message of ${message} and email is ${email}`);
+    const message1 = document.getElementById("contactUsMessage");
+    message1.classList.toggle("hidden");
+
+   
+};
+
